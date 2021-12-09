eb3c5601acac7736c450b45bbfe9831be33b5ff5
Mohammad Hamdaan <md.hamdaan19@gmail.com>
2021-12-09 15:43:31 +0530

    Modified stop_flying method

--- src/land_mav.py
+++ src/land_mav.py
@@ -6,16 +6,22 @@ import numpy as np
 from trajectory_msgs.msg import MultiDOFJointTrajectory
 from geometry_msgs.msg import PoseStamped
 from std_msgs.msg import Float32
+from mav_msgs.msg import Actuators
+import os
 
 class MakeTrajectory():
     def __init__(self):
         self.traj_publisher = rospy.Publisher("/firefly/command/trajectory", MultiDOFJointTrajectory, queue_size=5)
         self.point_publisher = rospy.Publisher("/firefly/command/pose", PoseStamped, queue_size=5)
-        self.motor_publisher0 = rospy.Publisher("/firefly/motor_speed/0", Float32, queue_size=5)
-        self.motor_publisher1 = rospy.Publisher("/firefly/motor_speed/1", Float32, queue_size=5)
-        self.motor_publisher2 = rospy.Publisher("/firefly/motor_speed/2", Float32, queue_size=5)
-        self.motor_publisher3 = rospy.Publisher("/firefly/motor_speed/3", Float32, queue_size=5)
-        self.motor_publisher4 = rospy.Publisher("/firefly/motor_speed/4", Float32, queue_size=5)
+
+        self.motor_publisher = rospy.Publisher("/firefly/command/motor_speed", Actuators, queue_size=5)
+
+        # self.motor_publisher0 = rospy.Publisher("/firefly/motor_speed/0", Float32, queue_size=1)
+        # self.motor_publisher1 = rospy.Publisher("/firefly/motor_speed/1", Float32, queue_size=1)
+        # self.motor_publisher2 = rospy.Publisher("/firefly/motor_speed/2", Float32, queue_size=1)
+        # self.motor_publisher3 = rospy.Publisher("/firefly/motor_speed/3", Float32, queue_size=1)
+        # self.motor_publisher4 = rospy.Publisher("/firefly/motor_speed/4", Float32, queue_size=1)
+        # self.motor_publisher5 = rospy.Publisher("/firefly/motor_speed/5", Float32, queue_size=1)
 
     def make_trajectory(self, start_point, goal_point):
         milestones = [start_point, goal_point]
@@ -31,17 +37,17 @@ class MakeTrajectory():
         self.traj_publisher.publish(traj_msg)
 
     def compute_next_point(self, current_point, goal_point):
-        mul_const = 1
+        mul_const = 1.0
         g_vector = goal_point - current_point
         mag_g_vector = np.linalg.norm(g_vector)
         unit_g_vector = g_vector/mag_g_vector
-        desired_point = mul_const*np.array([1.25,1.25,0.25])*unit_g_vector
+        desired_point = mul_const*np.array([1.30,1.30,0.25])*unit_g_vector
         
         global_desired_point = current_point + desired_point
 
         return global_desired_point
 
-    def publish_next_point(self, next_point):
+    def publish_point(self, next_point):
         next_point_msg = PoseStamped()
         next_point_msg.header.stamp = rospy.Time.now()
         next_point_msg.header.frame_id = "world"
@@ -55,14 +61,19 @@ class MakeTrajectory():
         self.point_publisher.publish(next_point_msg)
 
     def stop_flying(self):
-        motor_speed_msg = Float32()
+        motor_speed_msg = Actuators()
         
-        motor_speed_msg.data = 0
+        motor_speed_msg.header.stamp = rospy.Time.now()
+        motor_speed_msg.header.frame_id = 'firefly/base_link'
+        motor_speed_msg.angular_velocities = [0, 0, 0, 0, 0, 0]
+        motor_speed_msg.angles = []
+        motor_speed_msg.normalized = []
 
-        self.motor_publisher0.publish(motor_speed_msg)
-        self.motor_publisher1.publish(motor_speed_msg)
-        self.motor_publisher2.publish(motor_speed_msg)
-        self.motor_publisher3.publish(motor_speed_msg)
-        self.motor_publisher4.publish(motor_speed_msg)
+        os.system('rosnode kill /firefly/lee_position_controller_node')
+
+        self.motor_publisher.publish(motor_speed_msg)
+
+
+        rospy.loginfo("message published...")
 
     
